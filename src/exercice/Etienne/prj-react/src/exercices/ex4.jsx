5b413153d005d2800c6233af6362314fd9f6dff5
et-andre <et.andre@skynet.be>
2022-08-17 14:44:27 +0200

    ex4

---
+++ src/exercice/Etienne/prj-react/src/exercices/ex4.jsx
@@ -0,0 +1,79 @@
+import {useState} from "react";
+
+const initData = {
+    val1: "0",
+    oper: "add",
+    val2: "0",
+    res : "",
+    err : ""
+};
+
+const Ex4 = (props) =>
+{
+    const [data, setData] = useState(initData);
+
+    const ValChanged = (ec) =>
+    {
+        const { name, value, type } = ec.target;
+        const newVals = { ...data, [name]: value };
+        setData(newVals);
+    }
+
+    const formSubmit = (es) =>
+    {
+        es.preventDefault();
+        let res = 0;
+        let err = "";
+        if (data.oper === "add")
+        {
+            res = parseInt(data.val1) + parseInt(data.val2);
+        }
+        else if (data.oper === "min")
+        {
+            res = parseInt(data.val1) - parseInt(data.val2);
+        }
+        else if (data.oper === "mul")
+        {
+            res = parseInt(data.val1) * parseInt(data.val2);
+        }
+        else if (data.oper === "exp")
+        {
+            res = parseInt(data.val1) ** parseInt(data.val2);
+        }
+        else if (data.oper === "div")
+        {
+            if (!parseInt(data.val2))
+            {
+                err = "division par 0 impossible";
+            }
+            else
+            {
+                res = parseInt(data.val1) / parseInt(data.val2);
+                err = "";
+            }
+        }
+        setData({...data, res: res, err: err});
+    }
+
+    return (<>
+        <form onSubmit={formSubmit}>
+            <label htmlFor="val1">Nb1:</label>
+            <input id="val1" name="val1" type="number" value={data.val1} onChange={ValChanged}></input>
+            <label htmlFor="oper">Opération</label>
+            <select id="oper" name="oper" value={data.oper} onChange={ValChanged}>
+                <option value="add">+</option>
+                <option value="min">-</option>
+                <option value="mul">x</option>
+                <option value="div">/</option>
+                <option value="exp">^</option>
+            </select>
+            <label htmlFor="val2">Nb2:</label>
+            <input id="val2" name="val2" type="number" value={data.val2} onChange={ValChanged}></input>
+            <button type="submit">Calculer</button>
+            <input id="res"  name="res"  type="number" value={data.res } disabled></input>
+            {(data.err !== "") && <input id="err"  name="err"  type="textarea" value={data.err} disabled></input>}
+        </form>
+    </>);
+}
+
+export default Ex4;

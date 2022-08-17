ad1d9c5b73d89d0a0c09089f96347dea5f1850b6
et-andre <et.andre@skynet.be>
2022-08-17 10:01:54 +0200

    ex3

---
+++ src/exercice/Etienne/prj-react/src/exercices/ex3.jsx
@@ -0,0 +1,39 @@
+import {useState} from "react";
+import PropTypes from 'prop-types';
+
+const Ex3 = (props) =>
+{
+    const [cpt, setCpt] = useState(1);
+
+    const btnUpClick = () =>
+    {
+        setCpt(before => {
+            let bt = document.getElementById("btdown")
+            bt.removeAttribute("hidden");
+            bt.setAttribute   ("enabled", "");
+            return ++before;
+        })
+    }
+
+    const btnDnClick = () =>
+    {
+        setCpt(before => {
+            if (!--before)
+            {
+                let bt = document.getElementById("btdown")
+                bt.removeAttribute("enabled");
+                bt.setAttribute   ("hidden", "");
+            }
+            return before;
+        })
+    }
+
+    return (<>
+        <p> { cpt } </p>
+        <button onClick={btnUpClick}>Up</button>
+        <button onClick={btnDnClick} id="btdown">Down</button>
+    </>
+    );
+}
+
+export default Ex3;
